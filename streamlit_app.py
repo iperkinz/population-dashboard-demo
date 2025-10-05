bc05c969a71a9d1265dacc971891fada20a1b1aa
Ian Perkins <ian.perkins@gmail.com>
2025-10-05 04:30:16 +0000

    correct value parameter to stretch on line 258

--- streamlit_app.py
+++ streamlit_app.py
@@ -255,7 +255,7 @@ with col[2]:
     st.dataframe(df_selected_year_sorted,
                  column_order=("states", "population"),
                  hide_index=True,
-                 width=None,
+                 width=stretch,
                  column_config={
                     "states": st.column_config.TextColumn(
                         "States",
